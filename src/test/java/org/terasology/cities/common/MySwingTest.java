9570f5de177d517f0f1df282bdb59a2ff290e4c2
Martin Steiger <git@martin-steiger.de>
2014-01-15 18:26:40 +0100

    Fixed all CheckStyle warning except Javadoc

--- src/test/java/org/terasology/cities/common/MySwingTest.java
+++ src/test/java/org/terasology/cities/common/MySwingTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.terasology.cities.common;
 
@@ -10,6 +25,8 @@ import java.util.TimerTask;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+
+import org.terasology.cities.common.Touch.ThicknessMode;
 
 /**
  * TODO Type description
@@ -36,15 +53,27 @@ public class MySwingTest
 				
 				Touch murphy = new Touch() {
 					@Override
-					protected void  drawPixel(int x, int y, int aColor) {
-						g.setColor(new Color(aColor));
+					protected void  drawPixel(int x, int y, Color aColor) {
+						g.setColor(aColor);
 						g.drawLine(x, y, x, y);
 					}
 				};
 				
-				int mode = Touch.LINE_THICKNESS_MIDDLE;
-				murphy.drawThickLine(12, 12, 83, 44, 5, mode, 0x404000);
-				murphy.drawThickLineSimple(12, 12, 34, 78, 5, mode, 0x80);
+				ThicknessMode mode = Touch.ThicknessMode.MIDDLE;
+                Color black = new Color(0x000000);
+                Color gray = new Color(0x404040);
+
+                murphy.drawThickLine(22, 12, 83, 44, 5, mode, new Color(0x808000));
+				murphy.drawThickLineSimple(12, 52, 74, 28, 5, mode, new Color(0xA0));
+                murphy.drawThickLine(50, 70, 80, 70, 5, Touch.ThicknessMode.CLOCKWISE, gray);
+                murphy.drawThickLine(30, 70, 100, 70, 1, Touch.ThicknessMode.MIDDLE, black);
+                murphy.drawThickLine(50, 80, 80, 80, 5, Touch.ThicknessMode.COUNTERCLOCKWISE, gray);
+                murphy.drawThickLine(30, 80, 100, 80, 1, Touch.ThicknessMode.MIDDLE, black);
+                murphy.drawThickLine(50, 90, 80, 90, 5, Touch.ThicknessMode.MIDDLE, gray);
+                murphy.drawThickLine(30, 90, 100, 90, 1, Touch.ThicknessMode.MIDDLE, black);
+
+                murphy.drawThickLine(-15, 70, 10, 10, 8, Touch.ThicknessMode.MIDDLE, black);
+                
 //				murphy.drawLine(14, 12, 120, 18, 0xFFFF00FF);
 //				murphy.thickPerpBtnClick(new Point(4, 2), new Point(20, 8));
 			}
