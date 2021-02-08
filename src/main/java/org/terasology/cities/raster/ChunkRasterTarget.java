52b41ebc55b009b50a654661611ec92069534dcc
jdrueckert <jd.rueckert@googlemail.com>
2021-02-08 21:06:52 +0100

    chore: remove comment and update copyright (#47)

--- src/main/java/org/terasology/cities/raster/ChunkRasterTarget.java
+++ src/main/java/org/terasology/cities/raster/ChunkRasterTarget.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.cities.raster;
 
@@ -52,8 +39,6 @@ public class ChunkRasterTarget implements RasterTarget {
         int wx = chunk.getChunkWorldOffsetX();
         int wz = chunk.getChunkWorldOffsetZ();
         this.affectedArea = new BlockArea(wx, wz).setSize(chunk.getChunkSizeX(), chunk.getChunkSizeZ());
-
-//        this.affectedArea = Rect2i.createFromMinAndSize(wx, wz, chunk.getChunkSizeX(), chunk.getChunkSizeZ());
     }
 
     @Override
